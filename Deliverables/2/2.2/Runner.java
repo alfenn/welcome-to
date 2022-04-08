7bbe666c09bc808a4b7d13301fa4af77cd13381e
alfenn <52635707+alfenn@users.noreply.github.com>
2022-04-08 16:54:33 -0500

    move 2.2 from intelliJ into project directory

--- Deliverables/2/2.2/Runner.java
+++ Deliverables/2/2.2/Runner.java
@@ -1,17 +1,5 @@
-import org.json.jsonObject;
-
 public class Runner {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-
-    /**
-     * Your test driver should read 10 special JSON objects from
-     * STDIN, pass them to the back-end service, and display
-     * the result in STDOUT as a JSON array of 10 objects.
-     * @param JSON string
-     */
-    public static void driver(String json) {
-        json = "";
-    }
+    public static void main(String[] args) throws Exception {
+        Runner_2_2.run();
+    }    
 }
