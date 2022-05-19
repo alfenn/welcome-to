b685be6ae6b0f73e62c0ac2c2a0889dfcb7864d0
alfenn <52635707+alfenn@users.noreply.github.com>
2022-05-19 00:18:21 -0500

    whoops

--- Deliverables/3/3.1/test_driver.py
+++ Deliverables/3/3.1/test_driver.py
@@ -10,7 +10,7 @@ input_str = sys.stdin.read()
 # json.loads() takes a string
 j = json.loads(input_str)
 try:
-    PlayerState(j)
+    PlayerState(inp_ps=j)
     print(input_str)
 except:
     print("false")
