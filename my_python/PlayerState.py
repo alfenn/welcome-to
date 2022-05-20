07ab46ea9abd9eef352e4df046b35b5bf2042d71
alfenn <52635707+alfenn@users.noreply.github.com>
2022-05-20 02:16:54 -0500

    add num_agents and num_parks to check_effects_vs_built_houses

--- my_python/PlayerState.py
+++ my_python/PlayerState.py
@@ -50,6 +50,8 @@ class PlayerState:
 
     def _check_effects_vs_built_houses(self) -> None:
         num_fences_i = 0
+        num_agents_i = 0
+        num_parks_i = self.streets.parks
         for i in range(3):
             curr_street: Street = self.streets[i]
             for j in range(len(curr_street.homes)):
@@ -57,7 +59,10 @@ class PlayerState:
                 if j == 0:
                     if curr_house.l_fence.exists: num_fences_i += 1
                 if curr_house.r_fence.exists: num_fences_i += 1
-        if not num_fences_i <= self._help_total_non_bis_houses(): InvalidPlayerState("Number of fences is > number of non-bis houses")
+
+        for i in range(6):
+            num_agents_i += self.agents[i]
+        if not num_fences_i + num_agents_i + num_parks_i <= self._help_total_non_bis_houses(): InvalidPlayerState("Number of fences is > number of non-bis houses")
 
     def __eq__(self, other):
         # Check: agents
