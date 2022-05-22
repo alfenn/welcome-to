5065ad2913f9d6678f13cba4bebf2791563b93ed
alfenn <52635707+alfenn@users.noreply.github.com>
2022-05-22 14:25:18 -0500

    remove misc sum

--- my_python/PlayerState.py
+++ my_python/PlayerState.py
@@ -27,7 +27,7 @@ class PlayerState:
             self.streets.append(Street(inp_street=inp_ps["streets"][i]))
 
         ## Check: number of used effects <= built houses
-        self._check_effects_vs_built_houses()
+        # self._check_effects_vs_built_houses()
 
         ### If class fields are specified, set them directly
         try:
@@ -72,8 +72,8 @@ class PlayerState:
             num_agents_i += self.agents[i]
         for i in range(3):
             if self.city_plan_score[i] != "blank": num_claimed_plans_i += 1
-        # if not ((num_agents_i + num_claimed_plans_i + num_bis_i + num_parks_i + num_pools_i + num_temps_i + num_built_fences_i) <= self._help_total_non_bis_houses()): raise InvalidPlayerState("Number of effects is > number of non-bis houses")
-        if not ((num_built_fences_i) <= self._help_total_non_bis_houses()): raise InvalidPlayerState("Number of effects is > number of non-bis houses")
+        if not ((num_agents_i + num_claimed_plans_i + num_bis_i + num_parks_i + num_pools_i + num_temps_i + num_built_fences_i) <= self._help_total_non_bis_houses()): raise InvalidPlayerState("Number of effects is > number of non-bis houses")
+        # if not ((num_built_fences_i) <= self._help_total_non_bis_houses()): raise InvalidPlayerState("Number of effects is > number of non-bis houses")
 
     def __eq__(self, other):
         # Check: agents
