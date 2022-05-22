42a1caa32195b8835cb0f9815947a59c646fcac0
alfenn <52635707+alfenn@users.noreply.github.com>
2022-05-22 14:21:30 -0500

    remove everything but num built fences from misc sum

--- my_python/PlayerState.py
+++ my_python/PlayerState.py
@@ -73,7 +73,7 @@ class PlayerState:
         for i in range(3):
             if self.city_plan_score[i] != "blank": num_claimed_plans_i += 1
         # if not ((num_agents_i + num_claimed_plans_i + num_bis_i + num_parks_i + num_pools_i + num_temps_i + num_built_fences_i) <= self._help_total_non_bis_houses()): raise InvalidPlayerState("Number of effects is > number of non-bis houses")
-        if not ((num_bis_i + num_parks_i + num_pools_i + num_temps_i + num_built_fences_i) <= self._help_total_non_bis_houses()): raise InvalidPlayerState("Number of effects is > number of non-bis houses")
+        if not ((num_built_fences_i) <= self._help_total_non_bis_houses()): raise InvalidPlayerState("Number of effects is > number of non-bis houses")
 
     def __eq__(self, other):
         # Check: agents
