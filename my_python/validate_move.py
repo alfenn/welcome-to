27ff481e5fea9b06efe5b2a01a0b7e174af8534c
alfenn <52635707+alfenn@users.noreply.github.com>
2022-06-03 12:42:57 -0500

    Changes by priyu

--- my_python/validate_move.py
+++ my_python/validate_move.py
@@ -8,6 +8,8 @@ from my_python.exceptions import InvalidMove
 def validate_move(diff: PlayerState, ps1: PlayerState, ps2: PlayerState) -> None:
     ## Case: Cannot do nothing
     if ps1 == ps2: raise InvalidMove("The move can't be do nothing (must increment refusals).")
+    house_counter = 0
+    bis_counter = 0
     # Case: Newly built houses in ps2 cannot be already built in ps1
     for i in range(3):  # Iterate through the streets of both player states
         curr_street_1: Street = ps1.streets[i]
@@ -17,7 +19,8 @@ def validate_move(diff: PlayerState, ps1: PlayerState, ps2: PlayerState) -> None
             curr_house_2: House = curr_street_2.homes[j]
             ## Catch: built -> blank
             ##        built -> different built
-            ##        blank -> built
+            ##        built (bis) -> not bis but same number
+            ##        make sure only one house is being built unless it's a bis (ensure only one bis)
             if curr_house_1.num != curr_house_2.num:
                 ## Case: built -> blank => Error
                 if curr_house_1.is_built and not (curr_house_2.is_built): raise InvalidMove("A built house cannot go to a blank house")
@@ -25,6 +28,10 @@ def validate_move(diff: PlayerState, ps1: PlayerState, ps2: PlayerState) -> None
                 if curr_house_1.is_built and curr_house_2.is_built: raise InvalidMove("A built house cannot change nums")
                 ## Case: bis'd house -> non bis'd house => Error
                 if curr_house_1.is_bis and not (curr_house_2.is_bis): raise InvalidMove("A bis'd house cannot become a non bis'd house")
+                # If the new house is a bis, increment bis_counter
+                if curr_house_2.is_bis: bis_counter += 1
+                # If we're building a non-bis house...
+                if curr_house_2.is_built: house_counter += 1
     # loop through diff.non-bis-houses
     # if ps1[i] is not "blank" (aka. if ps1[i].built is not true) raise InvalidMove
     # check: make sure none of the existing house nums change/pools get removed/parks get removed/etc.
