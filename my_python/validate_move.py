cd12b0fcd2693475181270dfc114dae3560151b0
alfenn <52635707+alfenn@users.noreply.github.com>
2022-06-06 00:52:22 -0500

    1. fence validation 2. loop condition from .num->if house is different 3. check if newly built house is in gs.construction_cards

--- my_python/validate_move.py
+++ my_python/validate_move.py
@@ -9,6 +9,7 @@ from my_python.exceptions import InvalidMove
 def validate_move(diff: PlayerState, ps1: PlayerState, ps2: PlayerState, gs: GameState) -> None:
     ## Case: Cannot do nothing
     if ps1 == ps2: raise InvalidMove("The move can't be do nothing (must increment refusals).")
+    # Counter variables
     house_counter = 0
     effect_counter = 0
     house_num = None
@@ -34,10 +35,21 @@ def validate_move(diff: PlayerState, ps1: PlayerState, ps2: PlayerState, gs: Gam
                 if curr_house_1.is_built and curr_house_2.is_built: raise InvalidMove("A built house cannot change nums")
                 ## Case: bis'd house -> non bis'd house => Error
                 if curr_house_1.is_bis and not (curr_house_2.is_bis): raise InvalidMove("A bis'd house cannot become a non bis'd house")
-                # If the new house is a bis, increment bis_counter
-                if curr_house_2.is_bis: bis_counter += 1
+                ## Case-- valid: not built to built
+                #   Store newly built house num into house_num
+                if (not curr_house_1.is_built) and curr_house_2.is_built: house_num = curr_house_2.num
+                ##### Increment effect counter
+                # If we're building a bis...
+                if curr_house_2.is_bis: effect_counter += 1
                 # If we're building a non-bis house...
                 if curr_house_2.is_built: house_counter += 1
+                ###############
+                ## Fences: true fence becomes false fence
+                ###############
+                ## Case: true fence -> false fence
+                if curr_house_1.r_fence.exists and not curr_house_2.r_fence.exists: raise InvalidMove("A built fence cannot become an unbuilt fence")
+                ## If we're building fences...
+                if curr_house_2.r_fence.exists: effect_counter += 1
     # loop through diff.non-bis-houses
     # if ps1[i] is not "blank" (aka. if ps1[i].built is not true) raise InvalidMove
     # check: make sure none of the existing house nums change/pools get removed/parks get removed/etc.
@@ -45,7 +57,8 @@ def validate_move(diff: PlayerState, ps1: PlayerState, ps2: PlayerState, gs: Gam
     if not ((ps2.get_num_played_effects() - ps1.get_num_played_effects()) in {0, 1}): raise InvalidMove("Can't remove effects or play more than one effect.")
     ## Check: make sure the only valid combos for newly built houses are (1) 1 house + 1 bis and
     #                                                                    (2) 1 house
-    if bis_counter > 1: raise InvalidMove("Cannot build more than one newly bis'd house")
+    # Check: make sure that multiple fences are not being built on the same turn
+    if effect_counter > 1: raise InvalidMove("Cannot play more than one effect in a turn")
     if house_counter > 1: raise InvalidMove("Cannot build more than one new house")
     if effect_counter == 1 and house_counter == 0: raise InvalidMove("Cannot play effect without building a house")
     # check to make sure that house_num is in the construction cards
