45b18c35a4d744c33b3007a900b0f76f33c537f7
alfenn <52635707+alfenn@users.noreply.github.com>
2022-07-11 01:21:16 -0500

    priyanka magic

--- Deliverables/7/7.1/test_driver.py
+++ Deliverables/7/7.1/test_driver.py
@@ -4,7 +4,7 @@ sys.path.append('../../../')
 from my_python.PlayerState import PlayerState
 
 input_str = sys.stdin.read()
-# input_str = open('../../3/3.1/input_agents_g_1_robby.json', 'r').read()
+# input_str = open('../../3/3.1/input_bis_g_1_robby.json', 'r').read()
 j = json.loads(input_str)
 try:
     PlayerState(inp_ps=j)
