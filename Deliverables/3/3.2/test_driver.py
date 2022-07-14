8c17321d79fcc8ae191c88a1ef868e8dd64f1977
alfenn <52635707+alfenn@users.noreply.github.com>
2022-07-14 00:47:01 -0500

    finish 7.2

--- Deliverables/3/3.2/test_driver.py
+++ Deliverables/3/3.2/test_driver.py
@@ -3,16 +3,16 @@ import sys
 sys.path.append('../../../')
 from my_python.OldGameState import OldGameState
 
-# input_str = sys.stdin.read()
-input_str = open('input_city-plans_b_1_robby.json', 'r').read()
+input_str = sys.stdin.read()
+# input_str = open('input_city-plans_b_1_robby.json', 'r').read()
 
 # json.load() takes a filepath
 # json.loads() takes a string
 j = json.loads(input_str)
-# try:
-#     gs = OldGameState(j)
-#     print(gs)
-# except:
-#     print("false")
+try:
+    gs = OldGameState(j)
+    print(gs)
+except:
+    print("false")
 
-gs = OldGameState(j)
+# gs = OldGameState(j)
