95cccb0f6e2d102ebba1496b6d0b9c6455169bc1
alfenn <52635707+alfenn@users.noreply.github.com>
2022-07-22 13:49:09 -0500

    change 9.1 test driver to match 5.1

--- Deliverables/5/5.1/test_driver.py
+++ Deliverables/5/5.1/test_driver.py
@@ -1,22 +1,17 @@
 import sys, json
 sys.path.append('../../../')
-from my_python.OldGameState import OldGameState
+from my_python.GameState import GameState
 from my_python.PlayerState import PlayerState
-from my_python.MoveGeneratorInterface import MoveGeneratorInterface
+from my_python.MoveGeneratorInterface import MoveGenerator1
 
 inp_lst = json.loads(sys.stdin.read())
 # inp_lst = json.loads(open('input_00_robby.json', 'r').read())
 inp_gs = inp_lst[0]
 inp_ps = inp_lst[1]
-gs = OldGameState(inp_gs)
+gs = GameState(inp_gs)
 ps = PlayerState(inp_ps=inp_ps)
 
 # returns a player state: either with the valid move,
 #   or incrementing refusals.
-print(MoveGeneratorInterface(gs, ps))
-
-# open('test_output.json', 'w').\
-#    write(str(MoveGeneratorInterface(gs, ps)))
-
-# PlayerState(json.loads(str(MoveGeneratorInterface(gs,ps))))
+print(MoveGenerator1().generate(gs, ps))
 
--- Deliverables/9/9.1/test_driver.py
+++ Deliverables/9/9.1/test_driver.py
@@ -1,11 +1,17 @@
 import sys, json
 sys.path.append('../../../')
-from my_python.players_adaptors_translators import RemotePlayerAdapter
+from my_python.GameState import GameState
+from my_python.PlayerState import PlayerState
 from my_python.MoveGeneratorInterface import MoveGenerator1
 
+inp_lst = json.loads(sys.stdin.read())
+# inp_lst = json.loads(open('input_00_robby.json', 'r').read())
+inp_gs = inp_lst[0]
+inp_ps = inp_lst[1]
+gs = GameState(inp_gs)
+ps = PlayerState(inp_ps=inp_ps)
 
-network_config = json.loads(sys.stdin.read())
-HOST = network_config["host"]
-PORT = network_config["port"]
-rpa = RemotePlayerAdapter(MoveGenerator1(), HOST, PORT)
-rpa.play()
+# returns a player state: either with the valid move,
+#   or incrementing refusals.
+print(MoveGenerator1().generate(gs, ps))
+
