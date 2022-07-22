387364c00dd242491997571951ed3dfc88123194
alfenn <52635707+alfenn@users.noreply.github.com>
2022-07-22 15:50:17 -0500

    finish 9.1

--- Deliverables/3/3.2/test_driver.py
+++ Deliverables/3/3.2/test_driver.py
@@ -1,7 +1,7 @@
 import json
 import sys
 sys.path.append('../../../')
-from my_python.OldGameState import OldGameState
+from my_python.GameState import GameState
 
 input_str = sys.stdin.read()
 # input_str = open('input_city-plans_b_1_robby.json', 'r').read()
@@ -10,7 +10,7 @@ input_str = sys.stdin.read()
 # json.loads() takes a string
 j = json.loads(input_str)
 try:
-    gs = OldGameState(j)
+    gs = GameState(j)
     print(gs)
 except:
     print("false")
